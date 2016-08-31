bf07ddfdf62686c480fb6dc000ba50719ba0d075
xiechuanyu <1756569043@qq.com>
2016-08-31 11:17:35 +0800

    use LayoutManager to find last Item, perform better compatibility

---
+++ Library/build/generated/source/buildConfig/release/com/lhh/ptrrv/library/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.lhh.ptrrv.library;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.lhh.ptrrv.library";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1310;
+  public static final String VERSION_NAME = "1.3.1";
+}
